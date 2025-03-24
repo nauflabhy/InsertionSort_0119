e7a274f48246851bb6bfee93f910de26237e98f3
nauflabhy <n4uf4l.ku@gmail.com>
2025-03-24 21:07:50 +0800

    InsertionSort1

---
+++ InsertionSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input()
+{
+ 
+    while (true)
+    {
+        cout << "Masukan Jumlah Data pada Array :";
+        cin >> n;
+    }
+}
