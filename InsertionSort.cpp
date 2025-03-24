5ccb22d93417654b1a3bbd4bb24dfc21d4d61c83
nauflabhy <n4uf4l.ku@gmail.com>
2025-03-24 21:12:22 +0800

    InsertionSort3

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -21,4 +21,13 @@ void input()
             cout << "\nArray yang anda masukan maksimal 20 Elemen,\n";
         }
     }
+    cout << endl;
+    cout << "=======================" << endl;
+    cout << "Masukan Element Array" << endl;
+    cout << "=======================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
 }
